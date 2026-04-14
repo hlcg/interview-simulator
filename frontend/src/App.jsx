ab3fe0ef75e8425e3300a7f46ac49299b467a982
hlcg <53751639+hlcg@users.noreply.github.com>
2026-04-14 11:50:17 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import API from './api';
 import ConfigSession from './components/ConfigSession';
 import InterviewSession from './components/InterviewSession';
 import FinalReport from './components/FinalReport';
